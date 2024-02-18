f56d80e0542e6e0344837f77bb5eb6b49a8a8995
Manohar Kumar <manoharkumar371@gmail.com>
2024-02-18 12:54:01 +0530

    chmod permission generator ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
 
 function App() {
+  const [checkList, setCheckList] = useState([true, true, true, true, true, true, true, true, true]);
+  const [flag, setFlag] = useState("rwxrwxrwx");
+  const [octal, setOctal] = useState("777");
+
+  const generatePermissions = (input:boolean[]) => {
+    const permissions = input.map((value, index) => {
+        if (value) {
+            return index % 3 === 0 ? 'r' : index % 3 === 1 ? 'w' : 'x';
+        } else {
+            return '-';
+        }
+    }).join('');
+
+    const octalPermissions = parseInt(permissions.replace(/-/g, '0').replace(/[rwx]/g, '1'), 2).toString(8);
+
+    return [permissions, octalPermissions];
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    setCheckList((previous)=>{
+      let checkListUpdate = Array.from(previous);
+      checkListUpdate[index] = event.target.checked;
+      const [perm, octPerm] = generatePermissions(checkListUpdate);
+      setFlag(perm);
+      setOctal(octPerm);
+      return checkListUpdate;
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        CHMOD Simulator
       </header>
+      <h4>
+      {'chmod <options(-R/-v/-c/-f/-h)> <user_type(u/g/o/a)><add/revoke(+/-)><permission_type(r/w/x)> <file_or_directory name>'}
+      </h4>
+      <div id="permissionGrid">
+        <div className="action"></div>
+        <div className="action">Read</div>
+        <div className="action">Write</div>
+        <div className="action">Execute</div>
+
+        <div className="group">User</div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[0]}
+            onChange={(e)=>handleChange(e, 0)}
+          />
+        </div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[1]}
+            onChange={(e)=>handleChange(e, 1)}
+          />
+        </div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[2]}
+            onChange={(e)=>handleChange(e, 2)}
+          />
+        </div>
+
+        <div className="group">Group</div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[3]}
+            onChange={(e)=>handleChange(e, 3)}
+          />
+        </div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[4]}
+            onChange={(e)=>handleChange(e, 4)}
+          />
+        </div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[5]}
+            onChange={(e)=>handleChange(e, 5)}
+          />
+        </div>
+
+        <div className="group">Others</div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[6]}
+            onChange={(e)=>handleChange(e, 6)}
+          />
+        </div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[7]}
+            onChange={(e)=>handleChange(e, 7)}
+          />
+        </div>
+        <div className="checkbox">
+          <Checkbox 
+            checked={checkList[8]}
+            onChange={(e)=>handleChange(e, 8)}
+          />
+        </div>
+      </div>
+      <div className="output">
+        <TextField 
+          label="Flags"
+          value={flag}
+          margin="normal"
+          size="small"
+        />
+      </div>
+      <div className="output">
+        <TextField 
+          label="Octal"
+          value={octal}
+          margin="normal"
+          size="small"
+        />
+      </div>
     </div>
   );
 }
